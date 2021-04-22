bfb9eb81b9fa35136f4197160c3632fac597fa10
Maximilian Thoma <40450342+lanbugs@users.noreply.github.com>
2021-04-22 15:58:26 +0200

    Create lab5_task1_create_snapshot_of_tenant.py

---
+++ lab5_task1_create_snapshot_of_tenant.py
@@ -0,0 +1,48 @@
+import requests
+import json
+from pprint import pprint
+requests.packages.urllib3.disable_warnings()
+
+
+USERNAME = "admin"
+PASSWORD = "1234QWer"
+URL = "https://192.168.10.1/"
+
+# 1. Authentication
+payload = {
+	'aaaUser': {
+		'attributes': {
+			'name': USERNAME,
+            'pwd': PASSWORD
+            }
+        }
+}
+
+auth = requests.post(f'{URL}api/aaaLogin.json', data=json.dumps(payload), verify=False)
+COOKIES = auth.cookies
+
+# create snapshot of tenant
+
+DESCRIPTION = 'Lab4 end'
+TARGET_TENANT = 'APILAB-GUI'
+
+payload_task = {
+    'configExportP' : {
+        'attributes': {
+            'dn': 'uni/fabric/configexp-defaultOneTime',
+            'name': 'defaultOneTime',
+            'rn': 'configexp-defaultOneTime',
+            'snapshot': 'true',
+            'targetDn': f'uni/tn-{TARGET_TENANT}',
+            'adminSt': 'triggered',
+            'descr': DESCRIPTION,
+            'status': 'created,modified',
+        },
+        'children': []
+    }
+}
+
+result = requests.post(f'{URL}api/node/mo/uni/fabric/configexp-defaultOneTime.json', data=json.dumps(payload_task), verify = False, cookies=COOKIES)
+
+print('Return code: {code}'.format(code=result.status_code))
+pprint(result.content)
